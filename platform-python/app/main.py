4086af2de73d2866f5c8926cd3bfa8c3d8eb2e22
hieutrluu <thl1g15@outlook.com>
2024-04-22 17:23:27 +0700

    Update git ignore to remove .env and rename folder

--- platform-python/app/main.py
+++ platform-python/app/main.py
@@ -5,10 +5,32 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
+import logging
+
+# Add inngest for event driven architecture
+import inngest
+import inngest.fast_api
+# Create an Inngest client
+inngest_client = inngest.Inngest(
+    app_id="fast_api_example",
+    logger=logging.getLogger("uvicorn"),
+)
+
+
+# Create an Inngest function
+@inngest_client.create_function(
+    fn_id="my_function",
+    # Event that triggers this function
+    trigger=inngest.TriggerEvent(event="app/my_function"),
+)
+async def my_function(ctx: inngest.Context, step: inngest.Step) -> str:
+    ctx.logger.info(ctx.event)
+    return "done"
 
 
 def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
+    # return f"{route.tags[0]}-{route.name}"
+    return f"{route.name}"
 
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
@@ -20,6 +42,7 @@ app = FastAPI(
     generate_unique_id_function=custom_generate_unique_id,
 )
 
+inngest.fast_api.serve(app, inngest_client, [my_function])
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
